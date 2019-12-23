9ddcc432df12b0623bc40f1211e272870f5fe547
liuyan <liuyan@bstyjy.com>
2019-12-23 17:18:09 +0800

    完成接口文档描述项目中部分业务逻辑

---
+++ src/SelfPage_Service/PageInfo/ControllerInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace SelfPage_Service.PageInfo
+{
+    /// <summary>
+    /// 控制器信息
+    /// </summary>
+    public class ControllerInfo
+    {
+        /// <summary>
+        /// 控制器名称
+        /// </summary>
+        public string ControllerName = "";
+        /// <summary>
+        /// 控制器注释-描述信息
+        /// </summary>
+        public string Discribetion = "";
+        /// <summary>
+        /// 控制器的路由
+        /// </summary>
+        public string ControllerRoute = "";
+        /// <summary>
+        /// 控制器中所有Action
+        /// </summary>
+        public List<ActionInfo> Actions = new List<ActionInfo>();
+    }
+}
---
+++ src/SelfPage_Service/PageInfo/ResPageInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelfPage_Service.PageInfo
+{
+    /// <summary>
+    /// 返回的接口文档描述-分组页面信息
+    /// </summary>
+    public class ResPageInfo
+    {
+        /// <summary>
+        /// 分组接口文档描述页面中所有控制器
+        /// </summary>
+        public List<ControllerInfo> Controllers = new List<ControllerInfo>();
+        /// <summary>
+        /// 所在分组
+        /// </summary>
+        public string GroupName = "default";
+    }
+}
